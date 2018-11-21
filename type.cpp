14941afc4c7384753507b478742b47d48079d57f
crellian <31911863+crellian@users.noreply.github.com>
2018-11-21 17:59:33 -0500

    Update type.cpp

--- type.cpp
+++ type.cpp
@@ -1,3 +1,15 @@
+// Type for vector data
+// Type Vector is for int8_t
+template<class T> _Vector<T>::_Vector() {};
+template<class T> _Vector<T>::_Vector(T _len) :len(_len) { data=new T[_len]; for(int i=0; i<_len; i++) data[i]=0;}
+template<class T> _Vector<T>::_Vector(int _len, T val) :len(_len) { data=new T[_len]; for(int i=0; i<_len; i++) data[i]=val;}
+template<class T> _Vector<T>::_Vector(const T* _data, int _len) :data(_data), len(_len) {}
+template<class T> _Vector<T>::_Vector(const _Vector& v) :data(v.data), len(v.len) {}
+template<class T> _Vector<T>::_Vector& operator = (std::intializer_list<T> ls) { int id=0; for(auto item: ls) { this->data[id]=item; id++; } this->len = id; return *this; }
+template<class T> _Vector<T>::
+template<class T> _Vector<T>::
+template<class T> _Vector<T>::
+
 
 // Type for size data
 template<class T> _Size<T>::_Size() :width(0), height(0) {}
@@ -33,5 +45,3 @@ template<class T> T _Point<T>::dot_product(const _Point<T>& p1, const _Point<T>&
 template<class T> _Point3<T>& _Point<T>::cross_product(const _Point<T>& p1, const _Point<T>& p2) const {return ;} //
 
 template<class T> bool _Point<T>::inside(const _Rect<T>& r) const;//
-
-
